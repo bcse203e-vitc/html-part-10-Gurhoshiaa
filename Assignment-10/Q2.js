c1d3c4519f4c180163c3342798fa66def4fc77e9
Gurhoshiaa <gurhoshiaa.s2023@vitstudent.ac.in>
2025-02-21 13:19:39 +0530

    Add files via upload

---
+++ Assignment-10/Q2.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const redButton = document.getElementById('redBtn');
+    const blueButton = document.getElementById('blueBtn');
+
+    redButton.addEventListener('click', function() {
+        document.body.style.backgroundColor = 'red';
+    });
+
+    blueButton.addEventListener('click', function() {
+        document.body.style.backgroundColor = 'blue';
+    });
+});
